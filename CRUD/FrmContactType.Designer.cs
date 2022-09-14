a24abafd7683f1d87159009b0dbcfa02125290e8
Freddy Soto <fsoto@elcatador.com>
2022-09-13 22:58:36 -0400

    fixing bugs

---
+++ CRUD/FrmContactType.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace CRUD
+{
+    partial class FrmContactType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlForm = new System.Windows.Forms.Panel();
+            this.txtTypeDescription = new System.Windows.Forms.TextBox();
+            this.lblTypeDescription = new System.Windows.Forms.Label();
+            this.txtTypeName = new System.Windows.Forms.TextBox();
+            this.lblTypeName = new System.Windows.Forms.Label();
+            this.dgvContactTypes = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.pnlForm.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactTypes)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlForm
+            // 
+            this.pnlForm.Controls.Add(this.txtTypeDescription);
+            this.pnlForm.Controls.Add(this.lblTypeDescription);
+            this.pnlForm.Controls.Add(this.txtTypeName);
+            this.pnlForm.Controls.Add(this.lblTypeName);
+            this.pnlForm.Enabled = false;
+            this.pnlForm.Location = new System.Drawing.Point(12, 209);
+            this.pnlForm.Name = "pnlForm";
+            this.pnlForm.Size = new System.Drawing.Size(232, 113);
+            this.pnlForm.TabIndex = 73;
+            // 
+            // txtTypeDescription
+            // 
+            this.txtTypeDescription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTypeDescription.Location = new System.Drawing.Point(12, 89);
+            this.txtTypeDescription.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtTypeDescription.Name = "txtTypeDescription";
+            this.txtTypeDescription.Size = new System.Drawing.Size(173, 20);
+            this.txtTypeDescription.TabIndex = 69;
+            // 
+            // lblTypeDescription
+            // 
+            this.lblTypeDescription.AutoSize = true;
+            this.lblTypeDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTypeDescription.Location = new System.Drawing.Point(9, 70);
+            this.lblTypeDescription.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTypeDescription.Name = "lblTypeDescription";
+            this.lblTypeDescription.Size = new System.Drawing.Size(103, 13);
+            this.lblTypeDescription.TabIndex = 68;
+            this.lblTypeDescription.Text = "Type Description";
+            // 
+            // txtTypeName
+            // 
+            this.txtTypeName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTypeName.Location = new System.Drawing.Point(12, 36);
+            this.txtTypeName.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtTypeName.Name = "txtTypeName";
+            this.txtTypeName.Size = new System.Drawing.Size(173, 20);
+            this.txtTypeName.TabIndex = 67;
+            // 
+            // lblTypeName
+            // 
+            this.lblTypeName.AutoSize = true;
+            this.lblTypeName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTypeName.Location = new System.Drawing.Point(9, 17);
+            this.lblTypeName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTypeName.Name = "lblTypeName";
+            this.lblTypeName.Size = new System.Drawing.Size(71, 13);
+            this.lblTypeName.TabIndex = 66;
+            this.lblTypeName.Text = "Type Name";
+            // 
+            // dgvContactTypes
+            // 
+            this.dgvContactTypes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvContactTypes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContactTypes.Location = new System.Drawing.Point(11, 12);
+            this.dgvContactTypes.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.dgvContactTypes.Name = "dgvContactTypes";
+            this.dgvContactTypes.RowHeadersWidth = 51;
+            this.dgvContactTypes.RowTemplate.Height = 24;
+            this.dgvContactTypes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvContactTypes.Size = new System.Drawing.Size(430, 151);
+            this.dgvContactTypes.TabIndex = 71;
+            this.dgvContactTypes.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvContactTypes_CellClick);
+            this.dgvContactTypes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvContactTypes_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Location = new System.Drawing.Point(12, 169);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(232, 34);
+            this.panel1.TabIndex = 70;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(3, 4);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(140, 26);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Contact Type";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.btnAdd);
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.btnDelete);
+            this.panel2.Controls.Add(this.btnCancel);
+            this.panel2.Controls.Add(this.btnUpdate);
+            this.panel2.Location = new System.Drawing.Point(280, 169);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(161, 157);
+            this.panel2.TabIndex = 72;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(28, 13);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(101, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(28, 47);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(101, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.MistyRose;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(28, 131);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(101, 23);
+            this.btnDelete.TabIndex = 22;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Visible = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Location = new System.Drawing.Point(28, 76);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(101, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click_1);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.LightCyan;
+            this.btnUpdate.Location = new System.Drawing.Point(28, 105);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(101, 23);
+            this.btnUpdate.TabIndex = 23;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Visible = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click_1);
+            // 
+            // FrmContactType
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 330);
+            this.Controls.Add(this.pnlForm);
+            this.Controls.Add(this.dgvContactTypes);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "FrmContactType";
+            this.Text = "FrmContactType";
+            this.Load += new System.EventHandler(this.FrmContactType_Load);
+            this.pnlForm.ResumeLayout(false);
+            this.pnlForm.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactTypes)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlForm;
+        private System.Windows.Forms.TextBox txtTypeDescription;
+        private System.Windows.Forms.Label lblTypeDescription;
+        private System.Windows.Forms.TextBox txtTypeName;
+        private System.Windows.Forms.Label lblTypeName;
+        private System.Windows.Forms.DataGridView dgvContactTypes;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
